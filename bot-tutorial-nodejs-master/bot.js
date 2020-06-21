e92fe28e5d948e08115bcba33bc2d77cb3b6c948
dillnorm <31057595+dillnorm@users.noreply.github.com>
2020-06-20 21:28:01 -0400

    Update bot.js

--- bot-tutorial-nodejs-master/bot.js
+++ bot-tutorial-nodejs-master/bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 var botResponseGlobal;
 var health = 3;
 function respond() {
-  console.log(request);
+  console.log(JSON.parse(request));
   var request = JSON.parse(this.req.chunks[0]);
   botRegex = /cool guy/i;
   botRegexSup = /Sup/i;
